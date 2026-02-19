85a562113057f2d96848d81af261a94b1e887948
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2026-02-19 19:32:54 +0530

    Squashed 'stage-4-springboot/' changes from ad64fdd..c3fdb0a
    c3fdb0a chore(config): update application properties
    3246900 feat(security): enforce method-level RBAC on controllers
    98c75f1 fix(security): map database roles to authorities without prefix
    be25d54 refactor(spa): harden URL validation and secure delete-route handling
    86eaa50 feat(spa): implement SPA engine with History API and deep-linking support
    
    git-subtree-dir: stage-4-springboot
    git-subtree-split: c3fdb0a7c1652f10586ebf504a78272341964fb0

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -1,11 +1,44 @@
 const mainContent=document.getElementById('main-content');
 
 
-const ACTIVE_CLASS='nav-link.active';
+const ACTIVE_CLASS='nav-link-active';
 const HIDDEN_CLASS='nav-link-hidden';
 
-async function loadModule(moduleName,params={})
+async function loadModule(moduleName,params={}, push=true)
 {
+if(push)
+{
+    let url='/index.html';
+    if(moduleName==='employees')
+    {
+        url='/employees';
+    }else if(moduleName==='employee-form' && params.mode==='ADD')
+    {
+        url='/employees/add';
+    }else if(moduleName==='employee-form' && params.mode==='EDIT')
+    {
+        url=`/employees/edit/${params.id}`;
+    }else if(moduleName==='employee-delete-confirm')
+    {
+        url=`/employees/delete/${params.id}`;
+    }else if(moduleName==='designations')
+    {
+        url='/designations';
+    }else if(moduleName==='designation-form' && params.mode==='ADD')
+    {
+        url='/designations/add';
+    }else if(moduleName==='designation-form' && params.mode==='EDIT')
+    {
+        url=`/designations/edit/${params.id}`;
+    }else if(moduleName==='designation-delete-confirm')
+    {
+        url=`/designations/delete/${params.id}`;
+    }
+
+    //  Change the Browser URL (No Page Reload!)
+    //  save the 'moduleName' and 'params' in the history so the Back button works later.
+    window.history.pushState({ module: moduleName, params: params }, "", url);
+}
     if(moduleName==='HOME')
     {
         mainContent.innerHTML="<h1>Welcome</h1>"
@@ -30,7 +63,9 @@ async function loadModule(moduleName,params={})
         {
             controller.load(params);
         }
-        updateNavigation('designations');
+        const baseModule=moduleName.split('-')[0];
+        const navigationKey=baseModule.endsWith('s')?baseModule:baseModule+'s';
+        updateNavigation(navigationKey);
         }else
         {
             mainContent.innerHTML=`<h3 style="color:red">Error module '${moduleName}' not found</h3>`
@@ -71,9 +106,113 @@ if(linkDesignations)linkDesignations.classList.add(ACTIVE_CLASS);
  {
  if(linkEmployees)linkEmployees.classList.add(ACTIVE_CLASS);
  }
-
-
-
-
 }
 
+window.onpopstate=function(event)
+{
+    if(event.state)
+    {
+        // CRITICAL: i pass false here!
+        // "Load this module, but DO NOT touch the history stack."
+        // because if i call pushState again, i duplicate the entry(to understand more check the if logic of push)
+        loadModule(event.state.module,event.state.params,false);
+    }else
+    {
+        loadModule('HOME');
+    }
+};
+
+window.addEventListener('DOMContentLoaded',()=>{
+//    alert('DOMContentLoaded');
+    // look at the address bar
+    const currentPath=window.location.pathname;
+    console.log("page loaded, current path is ",currentPath);
+
+    if(currentPath==='/employees')
+    {
+        // Pass 'false' so we don't push the URL to history again (it's already there)
+        loadModule('employees',{},false);
+    }else if (currentPath === '/employees/add')
+    {
+        loadModule('employee-form', { mode: 'ADD' }, false);
+    }else if(currentPath.startsWith('/employees/edit'))
+    {
+        const pathIdInfo=parseEmployeeId(currentPath);
+        if(pathIdInfo.isValid)
+        {
+        loadModule('employee-form',{id : pathIdInfo.employeeId, mode: 'EDIT'},false);
+        }else
+        {
+            console.warn("Invalid Employee ID in URL. Redirecting...");
+            //Force the address bar to change to the safe URL immediately
+            window.history.replaceState(null, "", "/employees");
+            loadModule('employees',undefined,false);
+        }
+    }else if(currentPath.startsWith('/employees/delete'))
+    {
+            const pathIdInfo=parseEmployeeId(currentPath);
+                if(pathIdInfo.isValid)
+                {
+                loadModule('employee-delete-confirm',{id : pathIdInfo.employeeId},false);
+                }else
+                {
+                    console.warn("Invalid Employee ID in URL. Redirecting...");
+                    //Force the address bar to change to the safe URL immediately
+                    window.history.replaceState(null, "", "/employees");
+                    loadModule('employees',undefined,false);
+                }
+    }else if (currentPath === '/designations')
+    {
+        loadModule('designations', {}, false);
+    }
+    else if (currentPath === '/designations/add')
+    {
+        loadModule('designation-form', { mode: 'ADD' }, false);
+    }else if(currentPath.startsWith('/designations/edit'))
+    {
+        const pathCodeInfo=parseDesignationCode(currentPath);
+        if(pathCodeInfo.isValid)
+        {
+        loadModule('designation-form',{id : pathCodeInfo.code, mode: 'EDIT'},false);
+        }
+        else
+        {
+            console.warn("Invalid Designation Code in URL. Redirecting...");
+            //Force the address bar to change to the safe URL immediately
+            window.history.replaceState(null, "", "/designations");
+             loadModule('designations',undefined,false);
+        }
+    }else if(currentPath.startsWith('/designations/delete')){
+                const pathIdInfo=parseDesignationCode(currentPath);
+                    if(pathIdInfo.isValid)
+                    {
+                    loadModule('designation-delete-confirm',{id : pathIdInfo.code},false);
+                    }else
+                    {
+                        console.warn("Invalid Designation Code in URL. Redirecting...");
+                        //Force the address bar to change to the safe URL immediately
+                        window.history.replaceState(null, "", "/designations");
+                        loadModule('designations',undefined,false);
+                    }
+    }else
+    {
+        window.history.replaceState(null, "", "/");
+        loadModule('HOME', undefined, false);
+    }
+});
+
+function parseDesignationCode(currentPath)
+{
+const code=currentPath.split('/').pop();
+const isValid = code && /^[0-9]+$/.test(code);
+return {isValid,code};
+}
+function parseEmployeeId(currentPath)
+{
+        // we sprlit the URL by '/' and grad the last part using (.pop)
+        // "/employees/edit/A100005" -> splits into ["", "employees", "edit", "A100005"] -> pops "A100005"
+        const employeeId = currentPath.split('/').pop();
+
+        const isValid = employeeId && /^[a-zA-Z0-9]+$/.test(employeeId);
+        return {isValid,employeeId};
+}
